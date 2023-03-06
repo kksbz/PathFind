76717dd69d3ffd49fca1e7d76e13870c93d9555c
kksbz <hellghost7@naver.com>
2023-03-06 11:45:42 +0900

    [kks]Set canvas scale and Tile Prefab

--- PathFind/Assets/01.UnityProject/Scripts/Global/GFunc+Ui.cs
+++ PathFind/Assets/01.UnityProject/Scripts/Global/GFunc+Ui.cs
@@ -5,6 +5,16 @@ using TMPro;
 
 public static partial class GFunc
 {
+    //! 카메라 사이즈를 리턴하는 함수
+    public static Vector2 GetCameraSize()
+    {
+        Vector2 camerasize = Vector2.zero;
+        //하프사이즈라 2를 곱함
+        camerasize.y = Camera.main.orthographicSize * 2f;
+        camerasize.x = camerasize.y * Camera.main.aspect;
+        return camerasize;
+    } //GetCameraSize
+
     //! 텍스트메쉬프로 형태의 컴포넌트의 텍스트를 설정하는 함수
     public static void SetTmpText(this GameObject obj_, string text_)
     {
